033425cb6307445130d9a4d23c4a800774161243
Polly <apollinaria9203@yandex.by>
2021-10-16 10:10:18 +0300

    Add wish model

---
+++ Models/WishModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishList.Models
+{
+    public class WishModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Link { get; set; }
+    }
+}
